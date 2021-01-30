27e0373cb6be7dd8957418ffcfb5a1a3ba22111e
Letícia Pasdiora <leticiapasdiora@gmail.com>
2021-01-30 16:40:16 -0500

    added yin logic

---
+++ Assets/Scripts/ItemController.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemController : MonoBehaviour
+{
+    private bool _isLost;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.tag == "Yin")
+        {
+            Debug.Log("Amostra");
+            _isLost = true;
+        }
+        else
+        {
+            Debug.Log("Outra coisa entra");
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D col)
+    {
+        if (col.tag == "Yin")
+        {
+            Debug.Log("Xispa daqui");
+            _isLost = false;
+        }
+        else
+        {
+            Debug.Log("Outra coisa sai");
+        }
+    }
+}
