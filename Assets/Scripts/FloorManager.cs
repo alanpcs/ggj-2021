c639df805ed3d3ddb791d600d1b925dcf7eecffd
Letícia Pasdiora <leticiapasdiora@gmail.com>
2021-01-31 11:09:06 -0500

    fixed floor translation

--- Assets/Scripts/FloorManager.cs
+++ Assets/Scripts/FloorManager.cs
@@ -6,12 +6,17 @@ public class FloorManager : MonoBehaviour
 {
     private int _totalSocks;
     private int _lostSocks;
+    private bool _canChangePos;
+    private Vector3 _newPos;
+    private Vector3 _deltaPos;
+    private float translationSpeed = 4;
 
     void Start()
     {
         _totalSocks = 2;
         _lostSocks = 1;
         StartCoroutine(UpdatePosition());
+        _canChangePos = false;
     }
 
     // Update is called once per frame
@@ -19,6 +24,14 @@ public class FloorManager : MonoBehaviour
     {
         transform.Rotate(new Vector3(0, 0, -0.1f), Space.Self);
         UpdateSocksQty();
+        if (_canChangePos)
+        {
+            float move = translationSpeed * Time.deltaTime;
+            transform.position = Vector3.MoveTowards(transform.position, _deltaPos, move);
+            Vector3 remainingAnim = _newPos - transform.position;
+            if (remainingAnim.magnitude < 0.01f)
+                _canChangePos = false;
+        }
     }
 
     private void UpdateSocksQty()
@@ -32,21 +45,23 @@ public class FloorManager : MonoBehaviour
             if (isLost)
                 _lostSocks++;
         }
-        //Debug.Log("Total: " + _totalSocks + " Lost: " + _lostSocks);
     }
 
     IEnumerator UpdatePosition()
     {
         yield return new WaitForSeconds(1.0f);
 
+        _canChangePos = true;
+
         float balance = (float)_lostSocks / _totalSocks - 0.5f;
         // balance will range from -0.5f to 0.5f
-        Vector3 newPos = new Vector3(balance * 2 * 4.3f, transform.position.y, 0);
+        _deltaPos = new Vector3(balance * 2 * 4.3f, transform.position.y, 0);
 
-        float translationSpeed = 4;
-        float move = translationSpeed * Time.deltaTime;
+        //float translationSpeed = 4;
+        //float move = translationSpeed * Time.deltaTime;
 
-        transform.position = Vector3.MoveTowards(transform.position, newPos, move);
+        _newPos = transform.position + _deltaPos;
+
         Debug.Log("Total: " + _totalSocks + " Lost: " + _lostSocks + " Balance: " + balance);
 
         StartCoroutine(UpdatePosition());
